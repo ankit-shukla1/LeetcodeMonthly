06e2b543bb8ccdd822471ec5b5392876cdf91e51
Ankit Shukla <ankitshuklatiwari1@gmail.com>
2022-08-05 08:17:48 +0530

    Commit for 5/8

---
+++ August/5CombinationSum4.cpp
@@ -0,0 +1,16 @@
+// Problem Link : https://leetcode.com/problems/combination-sum-iv/https://leetcode.com/problems/combination-sum-iv/
+
+class Solution
+{
+public:
+    int combinationSum4(vector<int> &nums, int target)
+    {
+        vector<uint> dp(target + 1);
+        dp[0] = 1;
+        for (int curTarget = 1; curTarget <= target; curTarget++)
+            for (auto &num : nums)
+                if (num <= curTarget)
+                    dp[curTarget] += dp[curTarget - num];
+        return dp[target];
+    }
+};
