d4f4b565dda327e1593f956b8d904d77a0722ec8
Ankit Shukla <ankitshuklatiwari1@gmail.com>
2022-08-19 07:59:00 +0530

    Commit for 8/19

---
+++ August/19SplitIntoConsecutiveSequences.cpp
@@ -0,0 +1,35 @@
+// Problem Link : https://leetcode.com/problems/split-array-into-consecutive-subsequences/
+
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        
+        unordered_map<int, int> left, end;
+        
+        for(int i: nums)
+            left[i]++;
+        
+        for(int i : nums)
+        {
+            if(left[i]==0)
+                continue;
+            left[i]--;
+            if(end[i-1]>0)
+            {
+                end[i-1]--;
+                end[i]++;
+            }
+            else if(left[i+1]>0 and left[i+2]>0)
+            {
+                left[i+1]--;
+                left[i+2]--;
+                end[i+2]++;
+            }
+            else
+                return false;
+        }
+        
+        return true;
+        
+    }
+};
