b169d86dc07586ed3f1b2515db97dd821457fa1f
Ankit Shukla <ankitshuklatiwari1@gmail.com>
2022-09-07 07:44:57 +0530

    Commit for 9/7

---
+++ September/07BinaryTreeToString.cpp
@@ -0,0 +1,14 @@
+// Problem Link : https://leetcode.com/problems/construct-string-from-binary-tree/
+
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(!root)
+            return "";
+        string s= to_string(root->val);
+        if(root->left) s+="(" + tree2str(root->left) +")";
+        else if(root->right) s+="()";
+        if(root->right) s+="(" + tree2str(root->right) +")";
+        return s;
+    }
+};
