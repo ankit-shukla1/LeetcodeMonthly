2b4ee61a542061a903bfa3daeb53fc0c069080be
Ankit Shukla <ankitshuklatiwari1@gmail.com>
2023-01-01 11:23:17 +0530

    Commit for 1/1/23

---
+++ 2023/January/01WordPattern.cpp
@@ -0,0 +1,28 @@
+// Problem Link : https://leetcode.com/problems/word-pattern/
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        s.push_back(' ');
+        unordered_map<string, char> mp;
+        unordered_map<char, string> mp2;
+        string st;
+        int id = 0;
+        for(auto c: s) {
+            if(c == ' ') {
+                if(mp.find(st) == mp.end() and mp2.find(pattern[id]) == mp2.end()) {
+                    mp[st] = pattern[id];
+                    mp2[pattern[id]] = st;
+                }
+                else {
+                    if(mp[st] != pattern[id] or mp2[pattern[id]] != st) return false;
+                }
+                id++;
+                st.clear();
+            }
+            else st.push_back(c);
+        }
+        if(pattern.size() != id) return false;
+        return true;
+    }
+};
