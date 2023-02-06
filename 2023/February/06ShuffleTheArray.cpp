8a5c6191e2a872adcc59b8cb846c8b29aaa1fb32
Ankit Shukla <ankitshuklatiwari1@gmail.com>
2023-02-06 20:57:19 +0530

    Commit for 2/6

---
+++ 2023/February/06ShuffleTheArray.cpp
@@ -0,0 +1,16 @@
+// Problem Link : https://leetcode.com/problems/shuffle-the-array/description/
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> result;
+
+        for(int i=0; i<n; i++){
+            result.push_back(nums[i]);
+            result.push_back(nums[i+n]);
+        }
+
+        return result;
+        
+    }
+};
